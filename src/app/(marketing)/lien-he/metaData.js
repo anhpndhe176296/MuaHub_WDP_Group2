415d2cf5918f33b3818aac43b1873563159745ae
namlhhe163916 <namlhhe163916@fpt.edu.vn>
2025-11-06 12:52:45 +0700

    update ui

--- src/app/(marketing)/lien-he/metaData.js
+++ src/app/(marketing)/lien-he/metaData.js
@@ -1,14 +1,14 @@
 import { MAIN_URL_APP } from "@muahub/constants/MainContent";
 
 const META_DATA = {
-  TITLE: "MuaHub - Nền tảng đặt lịch Makeup chuyên nghiệp",
+  TITLE: "MuaHub - Đặt lịch Makeup",
   DESCRIPTION:
-    "MuaHub giúp bạn dễ dàng tìm và đặt lịch với chuyên gia trang điểm phù hợp. Trải nghiệm dịch vụ makeup nhanh chóng, tiện lợi và đáng tin cậy chỉ trong vài bước.",
+    "MuaHub là nền tảng đặt lịch makeup nhanh chóng và tiện lợi. Tìm chuyên gia trang điểm phù hợp và đặt lịch theo nhu cầu của bạn.",
   APPLICATION_NAME: "MuaHub",
-  GENERATOR: "Next.js",
+  GENERATOR: "NextJS",
   ROBOTS: "index, follow",
   IMAGE: [`${MAIN_URL_APP}/img/header-1.png`],
-  URL: "/",
+  URL: "/"
 };
 
 export default META_DATA;
