51d5d7553e13021c8bdc341371b2b46cc539f1ec
anhpndhe176296 <anhpndhe176296@fpt.edu.vn>
2025-11-12 12:23:31 +0700

    fix time tin dv

--- src/app/makeup-artists/(page)/timeline-dich-vu/page.js
+++ src/app/makeup-artists/(page)/timeline-dich-vu/page.js
@@ -24,7 +24,7 @@ const TimelineHistoryPage = () => {
     setLoading(true);
     try {
       const res = await SendRequest("GET", "/api/orders", {
-        ownerId: currentUser.role === ROLE_MANAGER.SALE ? currentUser.id : "",
+        ownerId: currentUser.role === ROLE_MANAGER.MUA ? currentUser.id : "",
         date: date
       });
       if (res.payload) {
@@ -61,8 +61,8 @@ const TimelineHistoryPage = () => {
   const handleCloseModal = () => {
     setSelectedBookings([]);
   };
-  console.log("services", services);
-  console.log("bookings", bookings);
+  // console.log("services", services);
+  // console.log("bookings", bookings);
   return (
     <PageContainer title="Lịch sử đặt dịch vụ" description="Danh sách các dịch vụ bạn đã đặt">
       <Box display="flex" justifyContent="space-between" alignItems="center" mb={3}>
